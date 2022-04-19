2f2ddf3eeb579f291d71b256cb94a5e1a0e45ad9
Ted McGinness <ttmcginness22@gmail.com>
2022-04-19 14:34:06 -0500

    added news search

--- src/components/News.js
+++ src/components/News.js
@@ -2,12 +2,16 @@ import React, {useState, useEffect} from 'react';
 import {Select, Typography, Row, Col, Avatar, Card} from 'antd';
 import moment from 'moment';
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi'
+import { useGetCryptosQuery } from '../services/cryptoApi'
 
 const {Text, Title} = Typography;
 const {Option} = Select;
+const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
+
 
 const News = ({simplified}) => {
     const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
+    const {data} = useGetCryptosQuery(100);
     const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
 
     if (!cryptoNews?.value) {
@@ -16,13 +20,36 @@ const News = ({simplified}) => {
     
     return (
         <Row gutter={[24, 24]}>
+            {!simplified && (
+                <Col span={24}>
+                    <Select
+                        showSearch
+                        className="select-news"
+                        placeholder="Select a Cryptocurrency"
+                        optionFilterProp="children"
+                        onChange={(value) => console.log(value)}
+                        filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) > 0}>
+                        <Option value="Cryptocurrency">Cryptocurrency</Option>
+                        </Select>
+                </Col>
+            )}
             {cryptoNews.value.map((news, i)=>(
                 <Col xs={24} sm={12} lg={8} key={i}>
                     <Card hoverable className="news-card">
                     <a href={news.url} target="_blank" rel="noreferrer" >
-                        <div className="news-data">
+                        <div className="news-image-container">
                             <Title className="news-title" level={4}>{news.name}</Title>
+                            <img style={{maxWidth: "200px", maxHeight: "100px"}} src={news?.image?.thumbnail?.contentUrl || demoImage} alt="news" />
                         </div>
+                        <p>
+                            {news.description > 100 ? `${news.description.substring(0, 100)} ...` : news.description
+                            }
+                        </p>
+                        <div className="provider-container">
+                            <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage}/>
+                            <Text className="provider-name">{news.provider[0]?.name}</Text>
+                        </div>
+                        <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
                     </a>
                     </Card>
                 </Col>
